cf8fcfde613dfa72a519f46c7f86b897ea3172a6
High23 <warenasb2@gmail.com>
2024-05-08 20:38:39 -0700

    Initial Commit

---
+++ models/item.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+    name: {type: String, required: true, minLength: 3, maxLength: 100}, 
+    company: {type: Schema.Types.ObjectId, ref: "Company", required: true, minLength: 3, maxLength: 100}, 
+    description: {type: Array, required: false, default: undefined}, 
+    category: [{type: Schema.Types.ObjectId, ref: "Category", required: false}], 
+    price: {type: Number, required: true}, 
+    numberInStock: {type: String, required: true} 
+})
+
+module.exports = mongoose.model("Item", ItemSchema)
