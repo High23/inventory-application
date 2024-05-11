fe889850df30aba65eb74940a8ace5b2a19ed951
High23 <warenasb2@gmail.com>
2024-05-10 19:28:53 -0700

    Adjusted game schema and added url virtual

--- models/game.js
+++ models/game.js
@@ -5,11 +5,15 @@ const Schema = mongoose.Schema;
 
 const GameSchema = new Schema({
     name: {type: String, required: true, minLength: 3, maxLength: 100}, 
-    company: {type: Schema.Types.ObjectId, ref: "Company", required: false, minLength: 3, maxLength: 100}, 
+    company: {type: Schema.Types.ObjectId, ref: "Company", required: true, minLength: 3, maxLength: 100}, 
     description: {type: Array, required: false, default: undefined}, 
-    category: [{type: Schema.Types.ObjectId, ref: "Category", required: false}], 
-    price: {type: Number, required: true}, 
+    category: [{type: Schema.Types.ObjectId, ref: "Category", required: true}], 
+    price: {type: Number, required: true, min: 0}, 
     numberInStock: {type: String, required: true} 
 })
 
-module.exports = mongoose.model("Item", GameSchema)
+GameSchema.virtual("url").get(function () {
+    return `/game/${this._id}`;
+})
+
+module.exports = mongoose.model("Game", GameSchema)
