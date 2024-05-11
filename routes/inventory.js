6452c19202eae7c6d94b81980c8f2ba17429df41
High23 <warenasb2@gmail.com>
2024-05-10 19:29:26 -0700

    Added game routes

--- routes/inventory.js
+++ routes/inventory.js
@@ -3,11 +3,28 @@ const router = express.Router();
 
 const category_controller = require("../controllers/categoryController")
 const company_controller = require("../controllers/companyController")
-const item_controller = require("../controllers/gameController")
+const game_controller = require("../controllers/gameController")
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/* Routes for games */
+
+router.get('/game/create', game_controller.game_create_get);
+
+router.post('/game/create', game_controller.game_create_post);
+
+router.get('/game/:id/delete', game_controller.game_delete_get);
+
+router.post('/game/:id/delete', game_controller.game_delete_post);
+
+router.get('/game/:id/update', game_controller.game_update_get);
+
+router.post('/game/:id/update', game_controller.game_update_post);
+
+router.get('/game/:id', game_controller.game);
+
+router.get('/games', game_controller.game_list);
+
+/* Routes for games */
+
+/* Routes for games */
 
 module.exports = router;
